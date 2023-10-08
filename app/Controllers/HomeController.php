d3a3e277a34a66df561f0799ff0ce5e75b9d4a37
Davinchi <johanarenas122@gmail.com>
2023-10-08 16:29:41 -0500

    Consulta basica en la tabla contactos

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,14 +1,18 @@
 <?php
 
 namespace App\Controllers;
+
+use App\Models\Contact;
 //Se hereda el metodo de la vista puesto que se tienen a reutilizar
 class HomeController extends Controller{
 
     public function index(){
+        $contactModel = new Contact();
+        return $contactModel->query('select * from contacts')->first();
         /*si se especifica home, es la ubicacion, pero si este se encuentra
         dentro de una carpeta hay que especificar*/
         return $this->view('home', [
-            'title' => 'Home',
+            'title' => 'mamabuevo',
             'description' => 'Esta es la página principal'
         ]);
     }
